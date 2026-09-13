b15c98e4662e3287fce10d2f56aed617f773a575
News_92 <almazpro0927@gmail.com>
2026-09-14 00:25:20 +0500

    feat(chess): connect outcome lesson visualizer

--- assets/chess-teacher-ui.js
+++ assets/chess-teacher-ui.js
@@ -12,3 +12,4 @@ document.write('<script src="assets/chess-trainer-access.js?v=2"><\\/script>');
 document.write('<script src="assets/chess-lesson-ui.js?v=2"><\\/script>');
 document.write('<script src="assets/chess-lesson-fix.js?v=1"><\\/script>');
 document.write('<script src="assets/chess-new-game-fix.js?v=1"><\\/script>');
+document.write('<script src="assets/chess-lesson-outcomes.js?v=1"><\\/script>');
