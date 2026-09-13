88b97c3003ba02a8c6081088240ec4907ddf1e8a
News_92 <almazpro0927@gmail.com>
2026-09-13 23:18:21 +0500

    Wire detailed chess review UI

--- assets/chess-teacher-ui.js
+++ assets/chess-teacher-ui.js
@@ -4,3 +4,4 @@ document.write('<script src="assets/chess-teacher-ui-core.js?v=5"><\/script>');
 document.write('<script src="assets/chess-game-modes.js?v=5"><\/script>');
 document.write('<script src="assets/chess-puzzle-coach.js?v=7"><\/script>');
 document.write('<script src="assets/chess-layout.js?v=2"><\/script>');
+document.write('<script src="assets/chess-review-ui.js?v=1"><\/script>');
